ada36d280bfeb2953d8f50856cfaf519522d64c0
Mihail Sazonkin <sazonkinmichail@gmail.com>
2023-10-29 19:41:56 +0300

    End - MainPage

--- Main_Site/js_custom_mouse/mouse.js
+++
@@ -1,5 +0,0 @@
-var mouseMove = document.querySelector('.mouse');
-document.addEventListener('mousemove', (le) => {
-  mouseMove.style.top=le.pageY + 'px'
-  mouseMove.style.left=le.pageX + 'px'
-});
