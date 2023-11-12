5d54752b65ff83216b03988e6e3525d02c5224e2
Mihail Sazonkin <sazonkinmichail@gmail.com>
2023-11-12 16:44:19 +0300

    add sign-in-up pages

---
+++ Main_Site/javascript/hoverButton.js
@@ -0,0 +1,15 @@
+function hoverOnLeft() {
+    document.getElementById('right').classList.add('active');
+}
+
+function hoverOffLeft() {
+    document.getElementById('right').classList.remove('active');
+}
+
+function hoverOnRight() {
+    document.getElementById('left').classList.add('active');
+}
+
+function hoverOffRight() {
+    document.getElementById('left').classList.remove('active');
+}
