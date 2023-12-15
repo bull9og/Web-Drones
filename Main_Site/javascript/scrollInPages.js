cb6723a9892bddb5806b15fa00313837f3703e06
Mihail Sazonkin <sazonkinmichail@gmail.com>
2023-12-15 13:22:26 +0300

    add custom scroll in all pages

---
+++ Main_Site/javascript/scrollInPages.js
@@ -0,0 +1,12 @@
+SmoothScroll({
+	animationTime: 600,
+	stepSize: 75,
+	accelerationDelta: 30,
+	accelerationMax: 2,
+	KeyboardSupport: true,
+	arrowScroll: 50,
+	pulseAlgorithm: true,
+	pulseScale: 4,
+	pulseNormalize: 1,
+	touchpadSupport: true,
+})
