e444d588c15cba9543b4a922488ec83de4a79fa5
Mihail Sazonkin <sazonkinmichail@gmail.com>
2024-05-31 19:27:19 +0300

    remake app

--- Main_Site/prilozhenie/click.js
+++ Main_Site/prilozhenie/click.js
@@ -1,6 +1,8 @@
 $(document).ready(function() {
-	$('.main__controlsContainer__button_clue').click(function(event) {
-		$('.main__controlsContainer__button_clue, .main__controlsContainer__button_clue_info').toggleClass('click');
-		$('.main__controlsContainer__button_clue, .main__controlsContainer__button_clue_info').remove('click');
+	$('.appContainer__FAQimg').click(function(event) {
+		$('appContainer__FAQimg').toggleClass('click');
+		$('appContainer__FAQimg').remove('click');
+		$(this).toggleClass('active').next().slideToggle(300);
+		$('.appContainer').toggleClass('darkScreen');
 	});
 });
---
+++ Main_Site/prilozhenie/keys.js
@@ -0,0 +1,96 @@
+document.addEventListener('keydown', function(event) {
+	switch (event.key) {
+	  case 'w':
+		// изменяем параметры и стили для клавиши "W"
+		document.getElementById('keyW').style.color = '#D9D9D9';
+		document.getElementById('arrow').style.fill = '#1E1E1E';
+		break;
+	  case 'a':
+		// изменяем параметры и стили для клавиши "A"
+		document.getElementById('keyA').style.color = '#D9D9D9';
+		document.getElementById('arrow2').style.fill = '#1E1E1E';
+		break;
+	  case 's':
+		// изменяем параметры и стили для клавиши "S"
+		document.getElementById('keyS').style.color = '#D9D9D9';
+		document.getElementById('arrow3').style.fill = '#1E1E1E';
+		break;
+	  case 'd':
+		// изменяем параметры и стили для клавиши "D"
+		document.getElementById('keyD').style.color = '#D9D9D9';
+		document.getElementById('arrow4').style.fill = '#1E1E1E';
+		break;
+	}
+  });
+  document.addEventListener('keyup', function(event) {
+	switch (event.key) {
+	  case 'w':
+		// изменяем параметры и стили для клавиши "W"
+		document.getElementById('keyW').style.color = 'transparent';
+		document.getElementById('arrow').style.fill = '#D9D9D9';
+		break;
+	  case 'a':
+		// изменяем параметры и стили для клавиши "A"
+		document.getElementById('keyA').style.color = 'transparent';
+		document.getElementById('arrow2').style.fill = '#D9D9D9';
+		break;
+	  case 's':
+		// изменяем параметры и стили для клавиши "S"
+		document.getElementById('keyS').style.color = 'transparent';
+		document.getElementById('arrow3').style.fill = '#D9D9D9';
+		break;
+	  case 'd':
+		// изменяем параметры и стили для клавиши "D"
+		document.getElementById('keyD').style.color = 'transparent';
+		document.getElementById('arrow4').style.fill = '#D9D9D9';
+		break;
+	}
+  });
+  document.addEventListener('keydown', function(event) {
+	switch (event.key) {
+	  case 'ц':
+		// изменяем параметры и стили для клавиши "W"
+		document.getElementById('keyW').style.color = '#D9D9D9';
+		document.getElementById('arrow').style.fill = '#1E1E1E';
+		break;
+	  case 'ф':
+		// изменяем параметры и стили для клавиши "A"
+		document.getElementById('keyA').style.color = '#D9D9D9';
+		document.getElementById('arrow2').style.fill = '#1E1E1E';
+		break;
+	  case 'ы':
+		// изменяем параметры и стили для клавиши "S"
+		document.getElementById('keyS').style.color = '#D9D9D9';
+		document.getElementById('arrow3').style.fill = '#1E1E1E';
+		break;
+	  case 'в':
+		// изменяем параметры и стили для клавиши "D"
+		document.getElementById('keyD').style.color = '#D9D9D9';
+		document.getElementById('arrow4').style.fill = '#1E1E1E';
+		break;
+	}
+  });
+  document.addEventListener('keyup', function(event) {
+	switch (event.key) {
+	  case 'ц':
+		// изменяем параметры и стили для клавиши "W"
+		document.getElementById('keyW').style.color = 'transparent';
+		document.getElementById('arrow').style.fill = '#D9D9D9';
+		break;
+	  case 'ф':
+		// изменяем параметры и стили для клавиши "A"
+		document.getElementById('keyA').style.color = 'transparent';
+		document.getElementById('arrow2').style.fill = '#D9D9D9';
+		break;
+	  case 'ы':
+		// изменяем параметры и стили для клавиши "S"
+		document.getElementById('keyS').style.color = 'transparent';
+		document.getElementById('arrow3').style.fill = '#D9D9D9';
+		break;
+	  case 'в':
+		// изменяем параметры и стили для клавиши "D"
+		document.getElementById('keyD').style.color = 'transparent';
+		document.getElementById('arrow4').style.fill = '#D9D9D9';
+		break;
+	}
+  });
