bd3e70180d6d97a736883239a2fb9ffabc700c91
Mihail Sazonkin <sazonkinmichail@gmail.com>
2024-06-20 22:42:51 +0300

    end app

---
+++ Main_Site/HTML_pages/keys.js
@@ -0,0 +1,403 @@
+document.addEventListener('keydown', function(event) {
+	switch (event.key) {
+	  case 'w':
+		// изменяем параметры и стили для клавиши "W"
+		document.getElementById('keyW').style.color = '#1E1E1E';
+		document.getElementById('keyW').style.transition = '100ms';
+		document.getElementById('keyW').style.backgroundColor = '#ADFF00';
+		document.getElementById('keyW').style.boxShadow = '0px 0px 15px 0px rgba(173, 255, 0, 0.60)';
+		break;
+	  case 'a':
+		// изменяем параметры и стили для клавиши "A"
+		document.getElementById('keyA').style.color = '#1E1E1E';
+		document.getElementById('keyA').style.transition = '100ms';
+		document.getElementById('keyA').style.backgroundColor = '#ADFF00';
+		document.getElementById('keyA').style.boxShadow = '0px 0px 15px 0px rgba(173, 255, 0, 0.60)';
+		break;
+	  case 's':
+		// изменяем параметры и стили для клавиши "S"
+		document.getElementById('keyS').style.color = '#1E1E1E';
+		document.getElementById('keyS').style.transition = '100ms';
+		document.getElementById('keyS').style.backgroundColor = '#ADFF00';
+		document.getElementById('keyS').style.boxShadow = '0px 0px 15px 0px rgba(173, 255, 0, 0.60)';
+		break;
+	  case 'd':
+		// изменяем параметры и стили для клавиши "D"
+		document.getElementById('keyD').style.color = '#1E1E1E';
+		document.getElementById('keyD').style.transition = '100ms';
+		document.getElementById('keyD').style.backgroundColor = '#ADFF00';
+		document.getElementById('keyD').style.boxShadow = '0px 0px 15px 0px rgba(173, 255, 0, 0.60)';
+		break;
+	}
+  });
+  document.addEventListener('keyup', function(event) {
+	switch (event.key) {
+	  case 'w':
+		// изменяем параметры и стили для клавиши "W"
+		document.getElementById('keyW').style.color = '#FFFFFF';
+		document.getElementById('keyW').style.transition = '100ms';
+		document.getElementById('keyW').style.backgroundColor = 'transparent';
+		document.getElementById('keyW').style.boxShadow = 'none';
+		break;
+	  case 'a':
+		// изменяем параметры и стили для клавиши "A"
+		document.getElementById('keyA').style.color = '#FFFFFF';
+		document.getElementById('keyA').style.transition = '100ms';
+		document.getElementById('keyA').style.backgroundColor = 'transparent';
+		document.getElementById('keyA').style.boxShadow = 'none';
+		break;
+	  case 's':
+		// изменяем параметры и стили для клавиши "S"
+		document.getElementById('keyS').style.color = '#FFFFFF';
+		document.getElementById('keyS').style.transition = '100ms';
+		document.getElementById('keyS').style.backgroundColor = 'transparent';
+		document.getElementById('keyS').style.boxShadow = 'none';
+		break;
+	  case 'd':
+		// изменяем параметры и стили для клавиши "D"
+		document.getElementById('keyD').style.color = '#FFFFFF';
+		document.getElementById('keyD').style.transition = '100ms';
+		document.getElementById('keyD').style.backgroundColor = 'transparent';
+		document.getElementById('keyD').style.boxShadow = 'none';
+		break;
+	}
+  });
+  document.addEventListener('keydown', function(event) {
+	switch (event.key) {
+	  case 'ц':
+		// изменяем параметры и стили для клавиши "W"
+		document.getElementById('keyW').style.color = '#1E1E1E';
+		document.getElementById('keyW').style.transition = '100ms';
+		document.getElementById('keyW').style.backgroundColor = '#ADFF00';
+		document.getElementById('keyW').style.boxShadow = '0px 0px 15px 0px rgba(173, 255, 0, 0.60)';
+		break;
+	  case 'ф':
+		// изменяем параметры и стили для клавиши "A"
+		document.getElementById('keyA').style.color = '#1E1E1E';
+		document.getElementById('keyA').style.transition = '100ms';
+		document.getElementById('keyA').style.backgroundColor = '#ADFF00';
+		document.getElementById('keyA').style.boxShadow = '0px 0px 15px 0px rgba(173, 255, 0, 0.60)';
+		break;
+	  case 'ы':
+		// изменяем параметры и стили для клавиши "S"
+		document.getElementById('keyS').style.color = '#1E1E1E';
+		document.getElementById('keyS').style.transition = '100ms';
+		document.getElementById('keyS').style.backgroundColor = '#ADFF00';
+		document.getElementById('keyS').style.boxShadow = '0px 0px 15px 0px rgba(173, 255, 0, 0.60)';
+		break;
+	  case 'в':
+		// изменяем параметры и стили для клавиши "D"
+		document.getElementById('keyD').style.color = '#1E1E1E';
+		document.getElementById('keyD').style.transition = '100ms';
+		document.getElementById('keyD').style.backgroundColor = '#ADFF00';
+		document.getElementById('keyD').style.boxShadow = '0px 0px 15px 0px rgba(173, 255, 0, 0.60)';
+		break;
+	}
+  });
+  document.addEventListener('keyup', function(event) {
+	switch (event.key) {
+	  case 'ц':
+		// изменяем параметры и стили для клавиши "W"
+		document.getElementById('keyW').style.color = '#FFFFFF';
+		document.getElementById('keyW').style.transition = '100ms';
+		document.getElementById('keyW').style.backgroundColor = 'transparent';
+		document.getElementById('keyW').style.boxShadow = 'none';
+		break;
+	  case 'ф':
+		// изменяем параметры и стили для клавиши "A"
+		document.getElementById('keyA').style.color = '#FFFFFF';
+		document.getElementById('keyA').style.transition = '100ms';
+		document.getElementById('keyA').style.backgroundColor = 'transparent';
+		document.getElementById('keyA').style.boxShadow = 'none';
+		break;
+	  case 'ы':
+		// изменяем параметры и стили для клавиши "S"
+		document.getElementById('keyS').style.color = '#FFFFFF';
+		document.getElementById('keyS').style.transition = '100ms';
+		document.getElementById('keyS').style.backgroundColor = 'transparent';
+		document.getElementById('keyS').style.boxShadow = 'none';
+		break;
+	  case 'в':
+		// изменяем параметры и стили для клавиши "D"
+		document.getElementById('keyD').style.color = '#FFFFFF';
+		document.getElementById('keyD').style.transition = '100ms';
+		document.getElementById('keyD').style.backgroundColor = 'transparent';
+		document.getElementById('keyD').style.boxShadow = 'none';
+		break;
+	}
+  });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  document.addEventListener('keydown', function(event) {
+	switch (event.key) {
+	  case 'r':
+		// изменяем параметры и стили для клавиши "W"
+		document.getElementById('keyR').style.color = '#1E1E1E';
+		document.getElementById('keyR').style.transition = '100ms';
+		document.getElementById('keyR').style.backgroundColor = '#ADFF00';
+		document.getElementById('keyR').style.boxShadow = '0px 0px 15px 0px rgba(173, 255, 0, 0.60)';
+		break;
+	  case 'q':
+		// изменяем параметры и стили для клавиши "A"
+		document.getElementById('keyQ').style.color = '#1E1E1E';
+		document.getElementById('keyQ').style.transition = '100ms';
+		document.getElementById('keyQ').style.backgroundColor = '#ADFF00';
+		document.getElementById('keyQ').style.boxShadow = '0px 0px 15px 0px rgba(173, 255, 0, 0.60)';
+		break;
+	  case 'f':
+		// изменяем параметры и стили для клавиши "S"
+		document.getElementById('keyF').style.color = '#1E1E1E';
+		document.getElementById('keyF').style.transition = '100ms';
+		document.getElementById('keyF').style.backgroundColor = '#ADFF00';
+		document.getElementById('keyF').style.boxShadow = '0px 0px 15px 0px rgba(173, 255, 0, 0.60)';
+		break;
+	  case 'e':
+		// изменяем параметры и стили для клавиши "D"
+		document.getElementById('keyE').style.color = '#1E1E1E';
+		document.getElementById('keyE').style.transition = '100ms';
+		document.getElementById('keyE').style.backgroundColor = '#ADFF00';
+		document.getElementById('keyE').style.boxShadow = '0px 0px 15px 0px rgba(173, 255, 0, 0.60)';
+		break;
+	}
+  });
+  document.addEventListener('keyup', function(event) {
+	switch (event.key) {
+	  case 'r':
+		// изменяем параметры и стили для клавиши "W"
+		document.getElementById('keyR').style.color = '#FFFFFF';
+		document.getElementById('keyR').style.transition = '100ms';
+		document.getElementById('keyR').style.backgroundColor = 'transparent';
+		document.getElementById('keyR').style.boxShadow = 'none';
+		break;
+	  case 'q':
+		// изменяем параметры и стили для клавиши "A"
+		document.getElementById('keyQ').style.color = '#FFFFFF';
+		document.getElementById('keyQ').style.transition = '100ms';
+		document.getElementById('keyQ').style.backgroundColor = 'transparent';
+		document.getElementById('keyQ').style.boxShadow = 'none';
+		break;
+	  case 'f':
+		// изменяем параметры и стили для клавиши "S"
+		document.getElementById('keyF').style.color = '#FFFFFF';
+		document.getElementById('keyF').style.transition = '100ms';
+		document.getElementById('keyF').style.backgroundColor = 'transparent';
+		document.getElementById('keyF').style.boxShadow = 'none';
+		break;
+	  case 'e':
+		// изменяем параметры и стили для клавиши "D"
+		document.getElementById('keyE').style.color = '#FFFFFF';
+		document.getElementById('keyE').style.transition = '100ms';
+		document.getElementById('keyE').style.backgroundColor = 'transparent';
+		document.getElementById('keyE').style.boxShadow = 'none';
+		break;
+	}
+  });
+  document.addEventListener('keydown', function(event) {
+	switch (event.key) {
+	  case 'к':
+		// изменяем параметры и стили для клавиши "W"
+		document.getElementById('keyR').style.color = '#1E1E1E';
+		document.getElementById('keyR').style.transition = '100ms';
+		document.getElementById('keyR').style.backgroundColor = '#ADFF00';
+		document.getElementById('keyR').style.boxShadow = '0px 0px 15px 0px rgba(173, 255, 0, 0.60)';
+		break;
+	  case 'й':
+		// изменяем параметры и стили для клавиши "A"
+		document.getElementById('keyQ').style.color = '#1E1E1E';
+		document.getElementById('keyQ').style.transition = '100ms';
+		document.getElementById('keyQ').style.backgroundColor = '#ADFF00';
+		document.getElementById('keyQ').style.boxShadow = '0px 0px 15px 0px rgba(173, 255, 0, 0.60)';
+		break;
+	  case 'а':
+		// изменяем параметры и стили для клавиши "S"
+		document.getElementById('keyF').style.color = '#1E1E1E';
+		document.getElementById('keyF').style.transition = '100ms';
+		document.getElementById('keyF').style.backgroundColor = '#ADFF00';
+		document.getElementById('keyF').style.boxShadow = '0px 0px 15px 0px rgba(173, 255, 0, 0.60)';
+		break;
+	  case 'у':
+		// изменяем параметры и стили для клавиши "D"
+		document.getElementById('keyE').style.color = '#1E1E1E';
+		document.getElementById('keyE').style.transition = '100ms';
+		document.getElementById('keyE').style.backgroundColor = '#ADFF00';
+		document.getElementById('keyE').style.boxShadow = '0px 0px 15px 0px rgba(173, 255, 0, 0.60)';
+		break;
+	}
+  });
+  document.addEventListener('keyup', function(event) {
+	switch (event.key) {
+	  case 'к':
+		// изменяем параметры и стили для клавиши "W"
+		document.getElementById('keyR').style.color = '#FFFFFF';
+		document.getElementById('keyR').style.transition = '100ms';
+		document.getElementById('keyR').style.backgroundColor = 'transparent';
+		document.getElementById('keyR').style.boxShadow = 'none';
+		break;
+	  case 'й':
+		// изменяем параметры и стили для клавиши "A"
+		document.getElementById('keyQ').style.color = '#FFFFFF';
+		document.getElementById('keyQ').style.transition = '100ms';
+		document.getElementById('keyQ').style.backgroundColor = 'transparent';
+		document.getElementById('keyQ').style.boxShadow = 'none';
+		break;
+	  case 'а':
+		// изменяем параметры и стили для клавиши "S"
+		document.getElementById('keyF').style.color = '#FFFFFF';
+		document.getElementById('keyF').style.transition = '100ms';
+		document.getElementById('keyF').style.backgroundColor = 'transparent';
+		document.getElementById('keyF').style.boxShadow = 'none';
+		break;
+	  case 'у':
+		// изменяем параметры и стили для клавиши "D"
+		document.getElementById('keyE').style.color = '#FFFFFF';
+		document.getElementById('keyE').style.transition = '100ms';
+		document.getElementById('keyE').style.backgroundColor = 'transparent';
+		document.getElementById('keyE').style.boxShadow = 'none';
+		break;
+	}
+  });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  document.addEventListener('keydown', function(event) {
+	switch (event.key) {
+	  case '-':
+		// изменяем параметры и стили для клавиши "W"
+		document.getElementById('keyMinus').style.color = '#1E1E1E';
+		document.getElementById('keyMinus').style.transition = '100ms';
+		document.getElementById('keyMinus').style.backgroundColor = '#ADFF00';
+		document.getElementById('keyMinus').style.boxShadow = '0px 0px 15px 0px rgba(173, 255, 0, 0.60)';
+		break;
+	  case 'v':
+		// изменяем параметры и стили для клавиши "A"
+		document.getElementById('keyV').style.color = '#1E1E1E';
+		document.getElementById('keyV').style.transition = '100ms';
+		document.getElementById('keyV').style.backgroundColor = '#ADFF00';
+		document.getElementById('keyV').style.boxShadow = '0px 0px 15px 0px rgba(173, 255, 0, 0.60)';
+		break;
+	  case '=':
+		// изменяем параметры и стили для клавиши "S"
+		document.getElementById('keyPlus').style.color = '#1E1E1E';
+		document.getElementById('keyPlus').style.transition = '100ms';
+		document.getElementById('keyPlus').style.backgroundColor = '#ADFF00';
+		document.getElementById('keyPlus').style.boxShadow = '0px 0px 15px 0px rgba(173, 255, 0, 0.60)';
+		break;
+	}
+  });
+  document.addEventListener('keyup', function(event) {
+	switch (event.key) {
+	  case '-':
+		// изменяем параметры и стили для клавиши "W"
+		document.getElementById('keyMinus').style.color = '#FFFFFF';
+		document.getElementById('keyMinus').style.transition = '100ms';
+		document.getElementById('keyMinus').style.backgroundColor = 'transparent';
+		document.getElementById('keyMinus').style.boxShadow = 'none';
+		break;
+	  case 'v':
+		// изменяем параметры и стили для клавиши "A"
+		document.getElementById('keyV').style.color = '#FFFFFF';
+		document.getElementById('keyV').style.transition = '100ms';
+		document.getElementById('keyV').style.backgroundColor = 'transparent';
+		document.getElementById('keyV').style.boxShadow = 'none';
+		break;
+	  case '=':
+		// изменяем параметры и стили для клавиши "S"
+		document.getElementById('keyPlus').style.color = '#FFFFFF';
+		document.getElementById('keyPlus').style.transition = '100ms';
+		document.getElementById('keyPlus').style.backgroundColor = 'transparent';
+		document.getElementById('keyPlus').style.boxShadow = 'none';
+		break;
+	}
+  });
+  document.addEventListener('keydown', function(event) {
+	switch (event.key) {
+	  case '-':
+		// изменяем параметры и стили для клавиши "W"
+		document.getElementById('keyMinus').style.color = '#1E1E1E';
+		document.getElementById('keyMinus').style.transition = '100ms';
+		document.getElementById('keyMinus').style.backgroundColor = '#ADFF00';
+		document.getElementById('keyMinus').style.boxShadow = '0px 0px 15px 0px rgba(173, 255, 0, 0.60)';
+		break;
+	  case 'м':
+		// изменяем параметры и стили для клавиши "A"
+		document.getElementById('keyV').style.color = '#1E1E1E';
+		document.getElementById('keyV').style.transition = '100ms';
+		document.getElementById('keyV').style.backgroundColor = '#ADFF00';
+		document.getElementById('keyV').style.boxShadow = '0px 0px 15px 0px rgba(173, 255, 0, 0.60)';
+		break;
+	  case '=':
+		// изменяем параметры и стили для клавиши "S"
+		document.getElementById('keyPlus').style.color = '#1E1E1E';
+		document.getElementById('keyPlus').style.transition = '100ms';
+		document.getElementById('keyPlus').style.backgroundColor = '#ADFF00';
+		document.getElementById('keyPlus').style.boxShadow = '0px 0px 15px 0px rgba(173, 255, 0, 0.60)';
+		break;
+	}
+  });
+  document.addEventListener('keyup', function(event) {
+	switch (event.key) {
+	  case '-':
+		// изменяем параметры и стили для клавиши "W"
+		document.getElementById('keyMinus').style.color = '#FFFFFF';
+		document.getElementById('keyMinus').style.transition = '100ms';
+		document.getElementById('keyMinus').style.backgroundColor = 'transparent';
+		document.getElementById('keyMinus').style.boxShadow = 'none';
+		break;
+	  case 'м':
+		// изменяем параметры и стили для клавиши "A"
+		document.getElementById('keyV').style.color = '#FFFFFF';
+		document.getElementById('keyV').style.transition = '100ms';
+		document.getElementById('keyV').style.backgroundColor = 'transparent';
+		document.getElementById('keyV').style.boxShadow = 'none';
+		break;
+	  case '=':
+		// изменяем параметры и стили для клавиши "S"
+		document.getElementById('keyPlus').style.color = '#FFFFFF';
+		document.getElementById('keyPlus').style.transition = '100ms';
+		document.getElementById('keyPlus').style.backgroundColor = 'transparent';
+		document.getElementById('keyPlus').style.boxShadow = 'none';
+		break;
+	}
+  });
